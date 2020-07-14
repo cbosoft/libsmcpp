d6ff3fa1028f536a37b222d3f3284c1e6ef0ed46
Christopher Boyle <chrismjboyle@gmail.com>
2020-07-14 12:29:30 +0100

    Add type to string helper func

--- src/util.cpp
+++ src/util.cpp
@@ -1,1 +1,14 @@
 #include "util.hpp"
+
+
+std::string type_to_str(uint32_t type)
+{
+    std::string s;
+    int shift = 24;
+    for (int i = 0; i < 4; i++)
+    {
+        s += (type >> shift) & 0xff;
+        shift -= 8;
+    }
+    return s;
+}
--- src/util.hpp
+++ src/util.hpp
@@ -1,1 +1,6 @@
 #pragma once
+
+#include <cstdint>
+#include <string>
+
+std::string type_to_str(uint32_t type);
