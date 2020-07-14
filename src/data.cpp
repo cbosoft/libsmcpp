7ef74cb40e8e3a1af2f34bf196f43d032f994262
Christopher Boyle <chrismjboyle@gmail.com>
2020-07-14 11:44:47 +0100

    Finish new export methods for SMC data

--- src/data.cpp
+++ src/data.cpp
@@ -99,132 +99,48 @@ double SMCData::from_flt_(uint8_t *data)
 
 
 
-void SMCData::get_data(uint32_t type, uint8_t **data, uint32_t &size) const
+void SMCData::get_data(uint32_t type, uint8_t *data, uint32_t &size) const
 {
 	// TODO: convert from data into requested type.
-	// data param must be allocd to > 4 bytes before passed into this method.
+
+	switch (type)
+	{
+		case TO_UINT(DATA_TYPE_FPE2):
+			this->to_fpe2(data, size);
+			break;
+
+		case TO_UINT(DATA_TYPE_SP78):
+			this->to_sp78(data, size);
+			break;
+
+		case TO_UINT(DATA_TYPE_FLT_):
+			this->to_flt_(data, size);
+			break;
+	}
 }
 
-void SMCData::set_fpe2(int i)
+void SMCData::to_fpe2(uint8_t *data, uint32_t &size) const
 {
-	this->data[0] = i >> 6;
-    this->data[1] = (i << 2) ^ (this->data[0] << 8);
-    this->dataSize = 2;
-    this->dataType = to_uint32_t(DATA_TYPE_FPE2);
+	size = 2;
+	int i = this->value;
+	data[0] = i >> 6;
+    data[1] = (i << 2) ^ (data[0] << 8);
 }
 
-void SMCData::set_flt(double d)
+void SMCData::to_flt_(uint8_t *data, uint32_t &size) const
 {
-	float f = float(d);
+	size = 4;
+	float f = float(this->value);
 	uint8_t *c_data = (uint8_t *)&f;
-
-	for (int i = 0; i < 4; i++) {
-		// need to flip?
-		this->data[i] = c_data[i];
-	}
-	this->dataSize = 4;
-	this->dataType = to_uint32_t(DATA_TYPE_FLT_);
+	memcpy(data, c_data, size);
 }
 
-void SMCData::set_sp78(double d)
+void SMCData::to_sp78(uint8_t *data, uint32_t &size) const
 {
-	uint8_t *c_data = (uint8_t *)&d;
-	memcpy(this->data, c_data, 8);
-	this->dataSize = 8;
-	this->dataType = TO_UINT(DATA_TYPE_SP78);
+	// TODO
+	(void) data;
+	(void) size;
 }
-
-
-
-
-
-double SMCData::from_fpe2(uint8_t *data) const
-{
-	int ans = 0;
-
-    // FPE2 assumedly means fixed point, with 2 exponent bits
-    // http://stackoverflow.com/questions/22160746/fpe2-and-sp78-data-types
-    ans += data[0] << 6;
-    ans += data[1] >> 2;// this is changed over upstream; is upstream broken? Or am I making a mistake in changing this?
-
-    int frac = data[1] & 3;
-
-
-    return ans;
-}
-
-double SMCData::int_from_sp78(uint8_t *data)
-{
-	double rv = data[0]; // no!
-	int ans = 0;
-
-	int sign = (data[0] >> 7) & 1;
-	ans += (data[0] & ((1 << 7) - 1)) << 6;
-	ans += data[]
-	return rv;
-}
-
-double SMCData::flt_from_flt(uint8_t *data)
-{
-	// fun bit of c-ish goodness
-	// pointer to the first location in the data array, cast to float*
-	// and dereferenced to get the float value
-	float f = *((float*)&(data[0]));
-	return double(f);
-}
-
-int SMCData::get_int_value() const
-{
-	switch (this->dataType) {
-		case TO_UINT(DATA_TYPE_FPE2):
-			return this->int_from_fpe2();
-
-		case TO_UINT(DATA_TYPE_SP78):
-			return int(this->flt_from_sp78());
-
-		case TO_UINT(DATA_TYPE_FLT_):
-			return int(this->flt_from_flt());
-	}
-
-	throw SMCError(Formatter() << "Unrecognised type: " << this->type_string());
-}
-
-double SMCData::get_float_value() const
-{
-	switch (this->dataType) {
-		case TO_UINT(DATA_TYPE_FPE2):
-			return double(this->int_from_fpe2());
-
-		case TO_UINT(DATA_TYPE_SP78):
-			return this->flt_from_sp78();
-
-		case TO_UINT(DATA_TYPE_FLT_):
-			return this->flt_from_flt();
-	}
-
-	throw SMCError(Formatter() << "Unrecognised type: " << this->type_string());
-}
-
-SMCData SMCData::cast_as_type(uint32_t type)
-{
-	int i = int(*this);
-	double f = double(*this);
-
-	switch (type) {
-		case TO_UINT(DATA_TYPE_FPE2):
-			return SMCData(i);
-
-		case TO_UINT(DATA_TYPE_FLT_):
-		case TO_UINT(DATA_TYPE_SP78):
-			return SMCData(f, type);
-
-	}
-
-	throw SMCError(Formatter() << "Unrecognised type: " << this->type_string());
-}
-
-
-
 
 
 
--- src/data.hpp
+++ src/data.hpp
@@ -15,23 +15,23 @@ private:
 	static double from_fpe2(uint8_t *data);
 	static double from_sp78(uint8_t *data);
 	static double from_flt_(uint8_t *data);
-	static void to_fpe2(int, uint8_t &*data, uint32_t &size);
-	static void to_sp78(int, uint8_t &*data, uint32_t &size);
-	static void to_flt_(double, uint8_t &*data, uint32_t &size);
+	void to_fpe2(uint8_t *data, uint32_t &size) const;
+	void to_sp78(uint8_t *data, uint32_t &size) const;
+	void to_flt_(uint8_t *data, uint32_t &size) const;
 
 	void set_value(double d);
-	void set_value(uint8_t *data, uint32_t type)
+	void set_value(uint8_t *data, uint32_t type);
 
 public:
 	SMCData();
 	SMCData(double f);
 	SMCData(uint8_t *data, uint32_t data_type);
 
-	get_data(uint32_t type, uint8_t &*data, uint32_t &size) const;
+	void get_data(uint32_t type, uint8_t *data, uint32_t &size) const;
 
 	operator double() const
 	{
-		return this->get_float_value();
+		return this->value;
 	}
 
 };
